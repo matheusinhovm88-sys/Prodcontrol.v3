b1aef140126c1438340c42aa0804dff55d83d730
MATHEUS B RIBEIRO <matheusinhovm88@gmail.com>
2026-08-20 21:07:31 -0300

    forca uso de variavel de ambiente firebase sem fallback local

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
 import os
 import json
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-from firebase_admin import credentials, initialize_app, firestore, storage
+from firebase_admin import credentials, initialize_app, firestore
 
 app = Flask(__name__)
 CORS(app)
 
-# Inicialização inteligente do Firebase (Funciona na Vercel e Local)
-if 'FIREBASE_SERVICE_ACCOUNT_JSON' in os.environ:
-    # Se estiver na Vercel, pega da variável de ambiente
-    key_dict = json.loads(os.environ.get('FIREBASE_SERVICE_ACCOUNT_JSON'))
-    cred = credentials.Certificate(key_dict)
-else:
-    # Se estiver rodando no seu computador localmente, usa o arquivo chave.json
-    cred = credentials.Certificate("chave.json")
-
+# Lê a chave diretamente da variável de ambiente da Vercel
+firebase_key = json.loads(os.environ.get('FIREBASE_SERVICE_ACCOUNT_JSON'))
+cred = credentials.Certificate(firebase_key)
 initialize_app(cred)
 db = firestore.client()
 
-# Suas rotas e o restante do seu código continuam aqui abaixo
 @app.route('/')
 def home():
     return jsonify({"status": "API rodando com sucesso!"})
